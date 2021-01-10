e0b92e9e1eda67844f22295c9f3da1ce19184c18
LashaMetreveli <lasha.metreveli.1@btu.edu.ge>
2021-01-10 12:13:58 +0400

    commit:

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b28350a79ab30cc0469b5adb7f241a56'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4fe71aed-2f7b-47b4-9a84-cae18acba84d'
+};
